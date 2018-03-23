6304d28b0355060387137062086fdf2dbf9847b5
Felix de Jesu <dcreatedark@gmail.com>
2018-03-22 21:47:06 -0600

    no message

---
+++ reloj.js
@@ -0,0 +1,31 @@
+const getRemainTime = deadline => {
+	let now = new Date(),
+	remainTime = (new Date(deadline) - now + 1000) / 1000;
+	remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2),
+	remainMinutes = ('0' + Math.floor(remainTime / 60 % 60)).slice(-2),
+	remainHours = ('0' + Math.floor(remainTime / 3600 % 24)).slice(-2),
+	remainDays = Math.floor(remainTime / (3600 * 24));
+
+	return {
+		remainSeconds,
+		remainMinutes,
+		remainHours,
+		remainDays
+	}
+};
+
+
+const countdown = (deadline, elem, finalMessage)=>{
+	const el= document.getElementById(elem);
+
+	const timerUpdate = setInterval( () => {
+		let t = getRemainTime(deadline);
+		el.innerHTML = `${t.remainDays} Dias ${t.remainHours}:${t.remainMinutes}:${t.remainSeconds} hrs.`;
+		if(t.remainTime <= 1){
+			clearInterval(timerUpdate);
+			el.innerHTML=finalMessage;
+		}
+	},1000)
+};
+
+countdown('Apr 07 2018 15:00:00 GMT-0600', 'clock', 'Feliz cumple');
