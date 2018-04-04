d8632bc3271da441294445689b03d13e9d639fb5
Felix de Jesus Carrillo Celerino <dcreatedark@gmail.com>
2018-04-04 12:31:33 -0500

    agregar mensaje

--- reloj.js
+++ reloj.js
@@ -28,4 +28,4 @@ const countdown = (deadline, elem, finalMessage)=>{
 	},1000)
 };
 
-countdown('Apr 07 2018 15:00:00 GMT-0500', 'clock', 'Feliz cumple');
+countdown('Apr 07 2018 15:00:00 GMT-0500', 'clock', 'Que empiece la Piñata');
